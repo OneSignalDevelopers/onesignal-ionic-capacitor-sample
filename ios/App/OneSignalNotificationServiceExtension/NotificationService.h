0607d12afc9e774b3f4da6c0ee8a495db931ac32
William Shepherd <will@codeincolor.io>
2022-09-19 15:16:00 -0500

    Integrate OneSignal into project

---
+++ ios/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationExtension
+//
+//  Created by William Shepherd on 9/19/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
